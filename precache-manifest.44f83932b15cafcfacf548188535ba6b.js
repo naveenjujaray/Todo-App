bfe37dd4659d63393d22b7ec17f9abab4e0c68b4
Jujaray Naveen <jujaraynaveen@gmail.com>
2020-08-02 00:30:46 +0530

    Updates

---
+++ precache-manifest.44f83932b15cafcfacf548188535ba6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c5474c2df73f948fb5c515c17a9227",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "6c487df9b93380333084",
+    "url": "/Todo-App/static/css/main.ecd26ae1.chunk.css"
+  },
+  {
+    "revision": "71e5e10dc1a69367bce1",
+    "url": "/Todo-App/static/js/2.97f72026.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Todo-App/static/js/2.97f72026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c487df9b93380333084",
+    "url": "/Todo-App/static/js/main.0503b9e9.chunk.js"
+  },
+  {
+    "revision": "32ae59bf0b31915a0ee0",
+    "url": "/Todo-App/static/js/runtime-main.33efac75.js"
+  }
+]);
