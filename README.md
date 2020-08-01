8395a3c068db85347d0b8ce733a87f93f7f2c0b1
Jujaray Naveen <jujaraynaveen@gmail.com>
2020-08-02 00:35:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Todo App ![react](https://img.shields.io/badge/React.js-16.2.0-blue) ![license](https://img.shields.io/github/license/naveenjujaray/Todo-App?style=plastic) ![forks](https://img.shields.io/github/forks/naveenjujaray/Todo-App?color=violet&label=Forks&style=plastic) ![Stars](https://img.shields.io/github/stars/naveenjujaray/Todo-App?color=yellow&label=Stars&style=plastic)
+ **[Remake From]**
+
+![Sample](src/todoapp.gif)
+
+### Features
+- Create a task
+- Mark as completed
+- Delete a task
+- Data saved through local storage 📦
+
+### Deployment
+In-order to work with this repo you will need to install Node.js on your machine.
+```
+# Clone this repository
+$ git clone https://github.com/naveenjujaray/Todo-App.git
+
+# Go into the repository
+$ cd Todo-App
+
+# Get Packages
+$ npm install
+
+# Launch/View
+$ npm start
+```
+
+[Remake From]: https://github.com/alyssaxuu/todo-app
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import ToDo, { TASK_STATUSES } from './components/ToDo';
+import './css/styles.css';
+import Emoji from 'react-emoji-render';
+
+export default class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			task: '',
+			list: ['Check it out in GitHub', 'Made with ❤️ by Alyssa X', 'Try making a new task above 👆', 'Build your own!'],
+			done: []
+		};
+		this.handleClick = this.handleClick.bind(this);
+		this.removeTodo = this.removeTodo.bind(this);
+		this.completeTodo = this.completeTodo.bind(this);
+	}
+	componentWillMount() {
+		const todo = localStorage.getItem('todo');
+		const done = localStorage.getItem('done');
+		if (todo !== null) {
+			this.setState({ list: JSON.parse(todo) });
+		}
+		if (done !== null) {
+			this.setState({ done: JSON.parse(done) });
+		}
+	}
+	onChange = (event) => {
+		this.setState({ task: event.target.value });
+	}
+	removeTodo(name, type) {
+		let array, index;
+		switch (type) {
+			case TASK_STATUSES.TO_DO: {
+				array = this.state.list;
+				index = array.indexOf(name);
+				array.splice(index, 1);
+				this.setState({ list: array });
+				localStorage.setItem('todo', JSON.stringify(array));
+			} break;
+			case TASK_STATUSES.DONE: {
+				array = this.state.done;
+				index = array.indexOf(name);
+				array.splice(index, 1);
+				this.setState({ done: array });
+				localStorage.setItem('done', JSON.stringify(array));
+			} break;
+			default: {
+				// nothing
+			} break;
+		}
+	}
+	completeTodo(name) {
+		this.removeTodo(name, TASK_STATUSES.TO_DO);
+		var join = this.state.done.slice();
+		join.push(name);
+		this.setState({ done: join });
+		localStorage.setItem('done', JSON.stringify(join));
+	}
+	handleClick() {
+		if (this.state.task !== '') {
+			this.setState({
+				task: '',
+				list: [...this.state.list, this.state.task]
+			}, () => {
+				localStorage.setItem('todo', JSON.stringify(this.state.list));
+				localStorage.setItem('done', JSON.stringify(this.state.done));
+			});
+		}
+	}
+	handleKey = (event) => {
+		if (event.key === 'Enter'){
+			this.handleClick();
+		}
+	}
+	render() {
+		return (
+			<div className="header center">
+				<h1><a id="link" href="https://naveenjujaray.js.org/">Click here for more knowledge</a></h1>
+				<h1>Todo List <Emoji text="🚦" /></h1>
+				<input placeholder="Add Task" maxLength={80} value={this.state.task} type='text' onKeyPress={this.handleKey} task={this.state.task} onChange={this.onChange}/>
+				<button onClick={this.handleClick}>+</button>
+				<ToDo tasks={this.state.list} done={this.state.done} remove={this.removeTodo} complete={this.completeTodo}/>
+			</div>
+		);
+	}
+}
